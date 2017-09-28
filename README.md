dc11ff8dd687538414a90111cb262b8dd4a98973
elchejin <chejochuleta09@gmail.com>
2017-09-28 12:06:52 -0600

    simplifying cycleCount function

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # nahuales
-Module to get the respective mayan sign of the Tzolkin calendar of a gregorian date.
+Module to get the respective mayan sign of the Tzolkin calendar of a gregorian calendar date.
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 'use strict';
 
-const nahuales = require('./lib/nahual-match');
+const nahual = require('./lib/nahual-match');
 
-const date = new Date('1988-12-27');
+// const date = new Date('1984-01-10');
+// const date = new Date('1988-12-27');
 
-console.log(nahuales.nahual(date));
+// console.log(nahualMatch.nahualMatcher(date));
+exports.nahual = nahual;
